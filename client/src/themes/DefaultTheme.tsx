5d75c97a63b51f9324db8bb162dc607d94575279
Marcela Portillo <00079419@uca.edu.sv>
2024-06-09 15:56:57 -0600

    Added style dependencies and default theme for client

---
+++ client/src/themes/DefaultTheme.tsx
@@ -0,0 +1,24 @@
+import { createTheme } from "@mui/material";
+import { TypographyStyleOptions } from "@mui/material/styles/createTypography";
+
+const headerStyleOptions: TypographyStyleOptions = {
+  fontFamily: "Barlow Condensed, Arial",
+  fontWeight: "bold",
+}
+
+const DefaultTheme = createTheme({
+  typography: {
+    fontFamily: "Montserrat, Arial",
+    h1: headerStyleOptions,
+    h2: headerStyleOptions,
+    h3: headerStyleOptions,
+    h4: headerStyleOptions,
+    h5: headerStyleOptions,
+    h6: headerStyleOptions,
+    button: {
+      fontFamily: "Barlow Condensed, Arial",
+    },
+  },
+});
+
+export default DefaultTheme;
